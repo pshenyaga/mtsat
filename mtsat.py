125657c516bc54e63d84558f0a5fa7a44024829e
Oleksii Pshenychnyi <afw@afw.net.ua>
2019-04-10 12:31:46 +0300

    config file format improved

--- mtsat.py
+++ mtsat.py
@@ -2,9 +2,27 @@
 
 import json
 
-config = "./dev_mtsat.conf"
+config = "./mtsat.conf"
 
-with open(config, 'r') as conf:
-    mt_config_l = json.load(conf)
+class mtsat:
+    groups = {}
+    routers = {}
+    
+    @classmethod
+    def parse_config(cls):
+        mt_config = []
+        with open(config, 'r') as conf:
+            mt_config = json.load(conf)
 
-print(mt_config_l)
+        cls.groups = mt_config.get("groups")
+        cls.routers = mt_config.get("routers")
+
+    @classmethod
+    def print_config(cls):
+        print(cls.groups)
+        print(cls.routers)
+
+if __name__ == "__main__":
+    mtsat.parse_config()
+    mtsat.print_config()
+
